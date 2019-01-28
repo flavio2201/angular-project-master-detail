77521a571018481ce1988be3467e70ec2010827f
Flavio Rocha <flavio.rocha@orangesistemas.com.br>
2019-01-27 22:30:00 -0300

    adding InMemoryDatabase for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,17 @@
+import {InMemoryDbService} from 'angular-in-memory-web-api';
+
+export class InMemoryDatabase implements InMemoryDbService {
+
+  createDb() {
+  const categories = [
+
+      { id: 1, name: 'Moradia', description: 'Pagamentos de contas da casa' },
+      { id: 2, name: 'Saúde', description: 'Plano de saude e Remedios' },
+      { id: 4, name: 'Lazer', description: 'Recebimentos de salarios' },
+      { id: 3, name: 'Salário', description: 'Recebimento de Salário' },
+      { id: 5, name: 'Freelas', description: 'Recebimentos de Freelas' }
+
+      ];
+      return { categories };
+    }
+}
