34c923dd29e3efab159285bd27b400b9fee8774d
Flavio Rocha <flavio.rocha@orangesistemas.com.br>
2019-02-07 21:08:35 -0300

    add category service

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,18 +1,16 @@
-import { Category } from './pages/shared/category.model';
+import { Category } from './pages/categories/shared/category.model';
 import {InMemoryDbService} from 'angular-in-memory-web-api';
 
 
 export class InMemoryDatabase implements InMemoryDbService {
 
   createDb() {
-  const categories : Category [] =  [
-
+  const categories: Category [] =  [
       { id: 1, name: 'Moradia', description: 'Pagamentos de contas da casa' },
       { id: 2, name: 'Saúde', description: 'Plano de saude e Remedios' },
       { id: 4, name: 'Lazer', description: 'Recebimentos de salarios' },
       { id: 3, name: 'Salário', description: 'Recebimento de Salário' },
       { id: 5, name: 'Freelas', description: 'Recebimentos de Freelas' }
-
       ];
       return { categories };
     }
--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,8 +1,8 @@
-export class Category{
+export class Category {
 
   constructor(
   public id?: number,
   public name?: string,
   public description?: string
-  ){}
+  ) {}
 }
