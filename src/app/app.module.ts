ef4adb1434f4b8db2932e71865c1ba6e3129643e
Flavio Rocha <flavio.rocha@orangesistemas.com.br>
2019-02-12 23:41:39 -0300

    project refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http/';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-
-import { InMemoryDatabase } from './in-memory-database';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
+    CoreModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,6 +1,5 @@
+import { SharedModule } from '../../shared/shared.module';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {ReactiveFormsModule} from '@angular/forms';
 import {CalendarModule} from 'primeng/calendar';
 import {IMaskModule} from 'angular-imask';
 
@@ -13,9 +12,8 @@ import {EntryFormComponent} from './entry-form/entry-form.component';
 @NgModule({
   declarations: [EntryListComponent, EntryFormComponent],
   imports: [
-    CommonModule,
+    SharedModule,
     EntriesRoutingModule,
-    ReactiveFormsModule,
     CalendarModule,
     IMaskModule
   ]
